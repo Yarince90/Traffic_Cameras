97b869b621f120a80c8a2b3c6d140c1c448cb55a
Yarince Vigil <Yarincevigil@hotmail.com>
2022-05-31 22:14:31 -0400

    Fixed image positioning - added comments describing code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
+//Returns a menu item <div> for a camera object
 buildMenuItem = (camera)=>{
     let menuItem = document.createElement('div');
-    
     menuItem.id = camera.number;
     menuItem.className = 'menu-item';
     menuItem.title = camera.name;
@@ -9,6 +8,7 @@ buildMenuItem = (camera)=>{
     return menuItem;
     }
     
+    //Creates camera menu items and registers a click handler
     buildMenu = function() {
         let nav = document.querySelector('nav');
 
@@ -17,54 +17,66 @@ buildMenuItem = (camera)=>{
             nav.appendChild(cameraMenuItem);
         });
 
+        //Adds a click handler
         nav.addEventListener('click', function(event){
+            //Find what camera ID was clicked
             let cameraNumber = event.target.id;
-
+            //Find camera obj based on ID
             let camera = trafficCameras.find(function(camera){
                 return camera.number === cameraNumber;
             });
+            //Updates img list and map based on camera clicked
             updateMap(camera.lat, camera.lng);
             updateImages(camera);
         });
     }
 
     function updateImages(camera){
+        //Set camera URL for #camera-image <img> element
         let cameraImage = document.querySelector('#camera-image');
+        //Get the URL from our camera via its prototype method
         cameraImage.src = camera.getImageURL();
 
+        //Do the same for all the direction image URLs
+        //Build a new <img> for each direction and place in the
+        //Directions container
         let directionsDiv = document.querySelector('#directions');
-
+        // Remove any existing content so we can add new data
         directionsDiv.innerHTML = '';
-
         let directionData = camera.getDirectionImages();
 
+        //Create a wrapper <div> to hold everything
         directionData.forEach(function(data){
             
             let div = document.createElement('div');
             div.className = 'directionCamera';
             
-            
+            // Create and put an <img> inside the wrapper <div>
             let img = document.createElement('img');
             img.src = data.url;
 
             div.appendChild(img);
 
+            //Create and add a <span> with the direction, add to <div>
             let span = document.createElement('span');
             span.innerText = data.direction.toUpperCase();
             div.appendChild(span);
 
+            // Put our wrapper <div> into the container <div> of directions
             directionsDiv.appendChild(div);
         });
 
+        // Remove .hidden class from our images div (if present)
         let imagesDiv = document.querySelector('#images');
         imagesDiv.classList.remove('hidden');
     }
     
+    // Keep references to map and marker, so we can move/update them
     let map;
     let marker;
 
     function updateMap(lat, lng) {
-
+        // Move or Create a marker to this intersection
         if(marker){
             marker.setLatLng({
                 lat: lat,
@@ -74,11 +86,14 @@ buildMenuItem = (camera)=>{
         else{
             marker = L.marker([lat, lng]).addTo(map);
         }
+        // Update the map's location
         map.setView([lat, lng]);
     }
-
+    
     function buildMap() {
         let senecaCoords = [43.7952, -79.3497];
+        
+        
         map = L.map('map').setView(senecaCoords, 16);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
